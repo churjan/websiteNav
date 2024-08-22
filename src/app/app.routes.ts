0513400bfa91323d544552714bc9e2f510e83709
Churjan <churjan@163.com>
2024-08-22 10:13:41 +0800

    feat:新增404页面

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { SpaceComponent } from './pages/space/space.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [{ path: '', component: SpaceComponent }];
+export const routes: Routes = [
+  { path: '', component: SpaceComponent, pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
